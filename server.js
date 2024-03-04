7dbae28cc59b0f5ec6fba1df3d817c19bff29ced
MuhammadUmar248 <umershafeeq053@gmail.com>
2024-03-04 06:09:53 -0800

    Upload server

---
+++ server.js
@@ -0,0 +1,454 @@
+import express from "express";
+import mongoose, { connect, version } from "mongoose";
+import "dotenv/config";
+import bcryptjs from "bcrypt";
+import { nanoid } from "nanoid";
+import jwt from "jsonwebtoken";
+import cors from "cors";
+import admin from "firebase-admin";
+import ServiceAccountKey from "./react-js-blog-website-54638-firebase-adminsdk-q6iqz-82deb4dbcd.json" assert { type: "json" };
+import { getAuth } from "firebase-admin/auth";
+
+import User from "./Schema/User.js";
+import Blog from "./Schema/Blog.js";
+
+const server = express();
+const PORT = 3001;
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(ServiceAccountKey),
+});
+
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+server.use(express.json());
+server.use(cors());
+
+mongoose
+  .connect(process.env.DB_Location, {
+    autoIndex: true,
+  })
+  .then((res) => {
+    console.log("DB connected!!");
+  })
+  .catch((err) => {
+    console.log("error while connting DB ", err);
+  });
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) {
+    return res.status(401).json({ error: "No access token" });
+  }
+  jwt.verify(token, process.env.SECRET_ACCESS_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Access token is invalid" });
+    }
+    req.user = user.id;
+    next();
+  });
+};
+
+const formatDatatoSend = (user) => {
+  const access_token = jwt.sign(
+    { id: user._id },
+    process.env.SECRET_ACCESS_KEY
+  );
+
+  return {
+    access_token,
+    profile_img: user.personal_info.profile_img,
+    username: user.personal_info.username,
+    fullname: user.personal_info.fullname,
+  };
+};
+
+const generateUsername = async (email) => {
+  let username = email.split("@")[0];
+
+  let isUsernameNotUnique = await User.exists({
+    "personal_info.username": username,
+  }).then((result) => result);
+  isUsernameNotUnique ? (username += nanoid().substring(0, 5)) : "";
+  return username;
+};
+
+server.post("/signup", (req, res) => {
+  console.log(req.body);
+
+  const { fullname, email, password } = req.body;
+
+  if (fullname.length < 3) {
+    return res
+      .status(403)
+      .json({ error: "Full name must be at least 3 letters long " });
+  }
+
+  if (!email.length) {
+    return res.status(403).json({ error: "Enter Email" });
+  }
+
+  if (!emailRegex.test(email)) {
+    return res.status(403).json({ error: "Email is invalid" });
+  }
+
+  if (!passwordRegex.test(password)) {
+    return res.status(403).json({
+      error:
+        "Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters",
+    });
+  }
+
+  bcryptjs.hash(password, 10, async (err, hashed_Password) => {
+    let username = await generateUsername(email);
+
+    const user = new User({
+      personal_info: {
+        fullname,
+        email,
+        password: hashed_Password,
+        username,
+      },
+    });
+    user
+      .save()
+      .then((u) => {
+        return res.status(200).json(formatDatatoSend(u));
+      })
+      .catch((err) => {
+        if (err.code == 11000) {
+          return res.status(500).json({ error: "Email already exists" });
+        }
+
+        return res.status(500).json({ error: err.message });
+      });
+  });
+});
+
+server.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    User.findOne({ "personal_info.email": email })
+      .then((user) => {
+        console.log("is user exist or not?->>>> ", user);
+
+        if (!user) {
+          return res.status(403).json({ error: "Email not found" });
+        }
+        console.log("my user ", user);
+
+        if (!user.google_auth) {
+          bcryptjs.compare(
+            password,
+            user.personal_info.password,
+            (err, result) => {
+              if (err) {
+                return res.status(403).json({
+                  error: "Error occured while login please try again",
+                });
+              }
+
+              if (!result) {
+                return res.status(403).json({ error: "Incorrect password" });
+              } else {
+                return res.status(200).json(formatDatatoSend(user));
+              }
+            }
+          );
+        } else {
+          return res.status(403).json({
+            error:
+              "Account was Created using google. Try logging in with google ",
+          });
+        }
+      })
+
+      .catch((err) => {
+        // console.log(err.message);
+        return res.status(500).json({ error: err });
+      });
+  } catch (error) {
+    console.log("something went wrong ", error);
+  }
+});
+
+server.post("/google-auth", async (req, res) => {
+  let { access_token } = req.body;
+
+  getAuth()
+    .verifyIdToken(access_token)
+    .then(async (decodecUser) => {
+      let { email, name, picture } = decodecUser;
+
+      picture = picture.replace("s96-c", "s384-c");
+
+      let user = await User.findOne({ "personal_info.email": email })
+        .select(
+          "personal_info.fullname personal_info.username personal_info.profile_img google_auth "
+        )
+        .then((u) => {
+          return u || null;
+        })
+        .catch((err) => {
+          return res.status(500).json({ error: err.message });
+        });
+
+      if (user) {
+        // login
+        if (!user.google_auth) {
+          return res.status(403).json({
+            error:
+              "This email was signed up without google. Please log in with password to access the account",
+          });
+        }
+      } else {
+        //sign up
+        let username = await generateUsername(email);
+
+        user = new User({
+          personal_info: {
+            fullname: name,
+            email,
+            username,
+          },
+          google_auth: true,
+        });
+        await user
+          .save()
+          .then((u) => {
+            user = u;
+          })
+          .catch((err) => {
+            return res.status(500).json({ error: err.message });
+          });
+      }
+
+      return res.status(200).json(formatDatatoSend(user));
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        error:
+          "Failed to authenticate you with google. Try with some other google account",
+      });
+    });
+});
+
+server.post("/latest-blogs", (req, res) => {
+  let { page } = req.body;
+
+  let maxLimit = 5;
+  Blog.find({ draft: false })
+    .populate(
+      "author",
+      "personal_info.profile_img personal_info.username personal_info.fullname -_id"
+    )
+    .sort({ publishedAt: -1 })
+    .select("blog_id title des banner activity tags  publishedAt -_id")
+    .skip((page - 1) * maxLimit)
+    .limit(maxLimit)
+    .then((blogs) => {
+      return res.status(200).json({ blogs });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/all-latest-blogs-count", (req, res) => {
+  Blog.countDocuments({ draft: false })
+    .then((count) => {
+      return res.status(200).json({ totalDocs: count });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.get("/trending-blogs", (req, res) => {
+  Blog.find({ draft: false })
+    .populate(
+      "author",
+      "personal_info.profile_img personal_info.username personal_info.fullname -_id"
+    )
+    .sort({
+      "activity.total_read": -1,
+      "activity.total_like": -1,
+      publishedAt: -1,
+    })
+    .select("blog_id title publishedAt -_id")
+    .limit(5)
+    .then((blogs) => {
+      return res.status(200).json({ blogs });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/search-blog", (req, res) => {
+  let { tag, page, query, author } = req.body;
+
+  let findQuery;
+
+  if (tag) {
+    findQuery = { tags: tag, draft: false };
+  } else if (query) {
+    findQuery = { draft: false, title: new RegExp(query, "i") };
+  } else if (author) {
+    findQuery = { author, draft: false };
+  }
+
+  let maxLimit = 2;
+  Blog.find(findQuery)
+    .populate(
+      "author",
+      "personal_info.profile_img personal_info.username personal_info.fullname -_id"
+    )
+    .sort({ publishedAt: -1 })
+    .select("blog_id title des banner activity tags  publishedAt -_id")
+    .skip((page - 1) * maxLimit)
+    .limit(maxLimit)
+    .then((blogs) => {
+      return res.status(200).json({ blogs });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/search-blog-count", (req, res) => {
+  let { tag, query, author } = req.body;
+  let findQuery;
+
+  if (tag) {
+    findQuery = { tags: tag, draft: false };
+  } else if (query) {
+    findQuery = { draft: false, title: new RegExp(query, "i") };
+  } else if (author) {
+    findQuery = { author, draft: false };
+  }
+
+  Blog.countDocuments(findQuery)
+    .then((count) => {
+      return res.status(200).json({ totalDocs: count });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/search-users", (req, res) => {
+  let { query } = req.body;
+
+  User.find({ "personal_info.username": new RegExp(query, "i") })
+    .limit(50)
+    .select(
+      "personal_info.fullname personal_info.username personal_info.porfile_img -_id"
+    )
+
+    .then((user) => {
+      return res.status(200).json({ user });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/get-profile", (req, res) => {
+  let { username } = req.body;
+
+  User.findOne({ "personal_info.username": username })
+    .select("-personal_info.password -google_auth -updateAt -blogs")
+    .then((user) => {
+      return res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/create-blog", verifyJWT, (req, res) => {
+  let authorId = req.user;
+
+  let { title, des, banner, tags, content, draft } = req.body;
+
+  if (!title.length) {
+    return res.status(403).json({ error: "You must Provide a title" });
+  }
+  if (!draft) {
+    if (!des.length || des.length > 200) {
+      return res.status(403).json({
+        error: "You must Provide a blog description under 200 character",
+      });
+    }
+    if (!banner.length) {
+      return res
+        .status(403)
+        .json({ error: "You must Provide blog banner to publish it" });
+    }
+    if (!content.blocks.length) {
+      return res
+        .status(403)
+        .json({ error: "There must be some blog content to publish it" });
+    }
+    if (!tags.length || tags.length > 10) {
+      return res.status(403).json({
+        error: "Provide tags in order to publish the blog, Maximum 10",
+      });
+    }
+  }
+
+  tags = tags.map((tag) => tag.toLowerCase());
+
+  let blog_id =
+    title
+      .replace(/[^a-zA-Z0-9]/g, "")
+      .replace(/\s+/g, "-")
+      .trim() + nanoid();
+  let blog = new Blog({
+    title,
+    des,
+    banner,
+    content,
+    tags,
+    author: authorId,
+    blog_id,
+    draft: Boolean(draft),
+  });
+
+  blog
+    .save()
+    .then((blog) => {
+      let incrementVal = draft ? 0 : 1;
+      User.findOneAndUpdate(
+        { _id: authorId },
+        {
+          $inc: { "account_ifo.total_posts": incrementVal },
+          $push: { blog: blog_id },
+        }
+      )
+        .then((user) => {
+          return res.status(200).json({ id: blog.blog_id });
+        })
+        .catch((err) => {
+          return res
+            .status(500)
+            .json({ error: "Falied to update total posts number" });
+        });
+    })
+    .catch((err) => {
+      return res.json(500).json({ error: err.message });
+    });
+});
+
+server.listen(PORT, () => {
+  console.log("Listening on Port : " + PORT);
+});
